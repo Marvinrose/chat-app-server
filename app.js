26aabb68b998f77605adfe2554a4e1b2371cf7b9
Chibuezem Marvinrose <rozzeymarvin32@gmail.com>
2024-03-01 15:17:27 +0100

    Created rateLimit and helmet

--- app.js
+++ app.js
@@ -1,5 +1,9 @@
 const express = require("express");
 
-const morgan = require("morgan");
+const morgan = require("morgan");  //  is a popular middleware for logging HTTP requests in Node.js web applications.
+
+const rateLimit = require("express-rate-limit"); //  Use to limit repeated requests to public APIs and/or endpoints such as password reset.
+
+const helmet = require("helmet"); // Helmet helps secure Express apps by setting HTTP response headers.
 
 const app = express();
