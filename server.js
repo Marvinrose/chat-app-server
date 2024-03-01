22d1385b9a884fccdcde19f8021fa2fd6e649800
Chibuezem Marvinrose <rozzeymarvin32@gmail.com>
2024-03-01 15:05:01 +0100

    Installed express and morgan

--- server.js
+++ server.js
@@ -2,5 +2,10 @@ const app = require("./app");
 
 const http = require("http");
 
-const server = http.createServer("app")
+const server = http.createServer(app);
 
+const port = 8000;
+
+server.listen(port, () => {
+  console.log(`App is running on port ${port}`);
+});
