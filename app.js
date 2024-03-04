70346be644b95a551e92ff0c4ad2bea0e9459741
Chibuezem Marvinrose <rozzeymarvin32@gmail.com>
2024-03-04 10:52:47 +0100

    Implemented uncaught exception and unhandled rejection in server.js

--- app.js
+++ app.js
@@ -16,6 +16,8 @@ const cors = require("cors"); // CORS is a node.js package for providing a Conne
 
 const app = express();
 
+// app.use(xss());
+
 app.use(express.json({ limit: "10kb" }));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
@@ -41,7 +43,7 @@ app.use(
 
 app.use(mongosanitize());
 
-app.use(xss());
+
 
 app.use(
   cors({
--- server.js
+++ server.js
@@ -1,5 +1,11 @@
 const app = require("./app");
 
+process.on("uncaughtException", (err) => {
+  console.log(err);
+  process.exit(1);
+});
+
+
 const http = require("http");
 
 const server = http.createServer(app);
@@ -8,4 +14,11 @@ const port = process.env.PORT || 8000;
 
 server.listen(port, () => {
   console.log(`App is running on port ${port}`);
+});
+
+process.on("unhandledRejection", (err) => {
+  console.log(err);
+  server.close(() => {
+    process.exit(1);
+  });
 });
